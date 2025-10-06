a624d35fabb7bfb22bff9b7ff61e59a9e8959b15
Ufuk Turuncoglu <turuncu@ucar.edu>
2025-10-06 16:23:27 -0600

    add graphcast examples

--- PythonSendCatalystRecv/data_graphcast.py
+++
@@ -1,7 +0,0 @@
-import os
-import xarray as xr
-import earth2studio
-from earth2studio.models.px import GraphCastSmall
-from earth2studio.data import WB2ERA5
-from earth2studio.io import NetCDF4Backend
-from earth2studio.run import deterministic as run
---
+++ PythonSendCatalystRecv/data_graphcast_small.py
@@ -0,0 +1,45 @@
+import os
+import xarray as xr
+from conduit import Node 
+import earth2studio
+from earth2studio.models.px import GraphCastSmall
+from earth2studio.data import WB2ERA5
+from earth2studio.io import XarrayBackend
+from earth2studio.run import deterministic as run
+
+# Arguments
+state = "export"
+channel = "atm"
+nx_atm = 360
+ny_atm = 181
+debug = True 
+
+# Access to channel
+#my_channel = my_node["channels/{}/{}".format(state, channel)]
+
+# Model specific parameters
+forecast = "2022-08-11T00:00:00" #my_node['state/time_str']
+step = 1 # lead time is 6h
+
+# Create model, dataset for input and also io object for output
+package = GraphCastSmall.load_default_package()
+model = GraphCastSmall.load_model(package)
+ds_input = WB2ERA5(cache=True)
+io = XarrayBackend()
+
+# Run model
+run([forecast], step, model, ds_input, io)
+
+# Save results
+if debug:
+    io.root.to_netcdf("{}_{}_{}.nc".format(state, channel, forecast))
+
+# Return node with data
+lead_time = 1
+my_node_return = Node()
+my_node_return.update(my_channel)
+my_node_return['data/fields/air_pressure_at_sea_level/values'] = io.root['msl'][0,lead_time].values.reshape(-1)
+my_node_return['data/fields/eastward_wind_at_10m_height/values'] = io.root['u10m'][0,lead_time].values.reshape(-1)
+my_node_return['data/fields/northward_wind_at_10m_height/values'] = io.root['v10m'][0,lead_time].values.reshape(-1)
+if debug:
+    my_node_return.save('my_node_return')
