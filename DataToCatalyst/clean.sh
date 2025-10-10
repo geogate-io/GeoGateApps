958842d18fb5c16eced113c17f68b76233edeb05
Ufuk Turuncoglu <turuncu@ucar.edu>
2025-10-10 13:08:35 -0600

    Revert "add data folder"
    This reverts commit 8a63031628ee7b5fe8dff04b2ab4638ad2787c0b.

--- DataToCatalyst/clean.sh
+++ DataToCatalyst/clean.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-rm -rf atm_0000* ocn_0000* datasets
-rm -rf PET* out err *.png 
-rm -rf ESMF_Profile.summary 
-rm -rf ATM_final_export.nc OCN_final_export.nc
+rm -rf atm_0000* ocn_0000* PET* out err *.png ESMF_Profile.summary ATM_final_export.nc OCN_final_export.nc datasets
